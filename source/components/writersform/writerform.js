ed44de99313419eae54cc0251eb3713570e15156
Harsh Shah <harsh.shah@sapientrazorfish.com>
2017-07-26 12:20:14 +0530

    Form Add & Style Changes\

--- source/components/writersform/writerform.js
+++ source/components/writersform/writerform.js
@@ -1,20 +1,31 @@
-module.exports = function (){
-	var input = $(".writerform .control-group input");
-	var label = $(".writerform .control-group label")
+module.exports = function() {
+    var input = $(".writerform .control-group input");
+    var label = $(".writerform .control-group label")
+    var check = $(".writerform .control-group input[type='checkbox']");
 
-	label.on('click',function(){
-		$(this).parent().find("input").focus();
-	});
+    label.on('click', function() {
+        $(this).parent().find("input").focus();
+    });
 
 
-	input.on('focus',function(){
-		$(this).parent().find("label").addClass("active");	
-	});
-	input.on('blur',function(){
-		if($(this).val().length == 0){
-		$(this).parent().find("label").removeClass("active");	
-		}
-	});	
+    input.on('focus', function() {
+        $(this).parent().find("label").addClass("active");
+    });
+    input.on('blur', function() {
+        if ($(this).val().length == 0) {
+            $(this).parent().find("label").removeClass("active");
+        }
+    });
 
-	
+
+    check.on('click', function() {
+        if ($(this).is(':checked')) {
+            $(this).parent().find("span").css("color", "#2196F3")
+        } else {
+            $(this).parent().find("span").css("color", "#999")
+        }
+
+    });
+
+
 }
